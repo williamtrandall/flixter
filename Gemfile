b248a38c5dddceedbae0f5605c6c231fe37e39fa
William Randall <williamtrandall@gmail.com>
2019-10-23 00:35:10 +0000

    Added Mini Magick

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ gem 'devise'
 gem 'carrierwave', '~> 2.0'
 gem 'figaro'
 gem 'fog-aws'
+
+gem 'mini_magick'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -24,6 +24,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
+  prcess resize_to_fill: [800, 350]
+
   # def scale(width, height)
   #   # do something
   # end
