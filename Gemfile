de81fc47abddd821f42852367eba65c8465fa97f
William Randall <williamtrandall@gmail.com>
2019-10-23 00:26:22 +0000

    Added Carrierwave/AWS

--- Gemfile
+++ Gemfile
@@ -72,3 +72,6 @@ end
 gem 'simple_form'
 
 gem 'devise'
+gem 'carrierwave', '~> 2.0'
+gem 'figaro'
+gem 'fog-aws'
--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -7,8 +7,8 @@ class Instructor::SectionsController < ApplicationController
   end
 
   def create
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
   private
@@ -18,7 +18,7 @@ class Instructor::SectionsController < ApplicationController
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
-  
+
   helper_method :current_course
   def current_course
     @current_course ||= Course.find(params[:course_id])
